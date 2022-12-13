c166b39998b2a2b9a2e9a56acf2d6d50d5b7a2c7
Abdikhan <melsnyambu@gmail.com>
2022-12-13 08:41:04 +0300

    First Commit
    First commit date-24/10/2022

---
+++ Database/commercial_farm.sql
@@ -0,0 +1,323 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 25, 2022 at 08:05 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `commercial farm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogdata`
+--
+
+CREATE TABLE `blogdata` (
+  `blogId` int(10) NOT NULL,
+  `blogUser` varchar(256) NOT NULL,
+  `blogTitle` varchar(256) NOT NULL,
+  `blogContent` longtext NOT NULL,
+  `blogTime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `likes` int(10) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `blogdata`
+--
+
+INSERT INTO `blogdata` (`blogId`, `blogUser`, `blogTitle`, `blogContent`, `blogTime`, `likes`) VALUES
+(19, 'ThePhenom', 'First Blog', '<p>Its Awesome website<img alt=\"wink\" src=\"https://cdn.ckeditor.com/4.8.0/full/plugins/smiley/images/wink_smile.png\" style=\"height:23px; width:23px\" title=\"wink\" /></p>\r\n', '2018-02-25 13:09:41', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogfeedback`
+--
+
+CREATE TABLE `blogfeedback` (
+  `blogId` int(10) NOT NULL,
+  `comment` varchar(256) NOT NULL,
+  `commentUser` varchar(256) NOT NULL,
+  `commentPic` varchar(256) NOT NULL DEFAULT 'profile0.png',
+  `commentTime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `blogfeedback`
+--
+
+INSERT INTO `blogfeedback` (`blogId`, `comment`, `commentUser`, `commentPic`, `commentTime`) VALUES
+(19, 'Mast yarr', 'ThePhenom', 'profile0.png', '2018-02-25 13:09:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buyer`
+--
+
+CREATE TABLE `buyer` (
+  `bid` int(100) NOT NULL,
+  `bname` varchar(100) NOT NULL,
+  `busername` varchar(100) NOT NULL,
+  `bpassword` varchar(100) NOT NULL,
+  `bhash` varchar(100) NOT NULL,
+  `bemail` varchar(100) NOT NULL,
+  `bmobile` varchar(100) NOT NULL,
+  `baddress` text NOT NULL,
+  `bactive` int(100) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farmer`
+--
+
+CREATE TABLE `farmer` (
+  `fid` int(255) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `fusername` varchar(255) NOT NULL,
+  `fpassword` varchar(255) NOT NULL,
+  `fhash` varchar(255) NOT NULL,
+  `femail` varchar(255) NOT NULL,
+  `fmobile` varchar(255) NOT NULL,
+  `faddress` text NOT NULL,
+  `factive` int(255) NOT NULL DEFAULT 0,
+  `frating` int(11) NOT NULL DEFAULT 0,
+  `picExt` varchar(255) NOT NULL DEFAULT 'png',
+  `picStatus` int(10) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `farmer`
+--
+
+INSERT INTO `farmer` (`fid`, `fname`, `fusername`, `fpassword`, `fhash`, `femail`, `fmobile`, `faddress`, `factive`, `frating`, `picExt`, `picStatus`) VALUES
+(3, 'Lydia Mels', 'ThePhenom', '$2y$10$22ezmzHRa9c5ycHmVm5RpOnlT4LwFaDZar1XhmLRJQKGrcVRhPgti', '61b4a64be663682e8cb037d9719ad8cd', 'kmendki98@gmail.com', '8600611198', 'abcde', 0, 0, 'png', 0),
+(4, 'Narasha Mepukori', 'hailey', '$2y$10$7QVBKMVm4p/d0AOCVauhQOdHacpyeVlOgKzROuVO8bGwZc7v7ZjVi', '2291d2ec3b3048d1a6f86c2c4591b7e0', 'nara@gmail.com', '9007137711', 'Thika', 0, 0, 'png', 0),
+(5, 'lydia', 'melslydia', '$2y$10$/unUUt/gdAQMn.bHC4Y4BuUuZ3DM6HjcNc/vI/EoE3/lpA2oUwutm', 'f8c1f23d6a8d8d7904fc0ea8e066b3bb', 'mels@gmail.com', '0791524062', 'ruiru', 0, 0, 'png', 0),
+(6, 'Stanley', 'stano091', '$2y$10$HVEDBo69cuATeYtWqJKJXexfh36.N8tdADcggJyaSiMXNWMbvNqum', '3988c7f88ebcb58c6ce932b957b6f332', 'stanley@gmail.com', '0702884698', 'makongeni', 0, 0, 'png', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fproduct`
+--
+
+CREATE TABLE `fproduct` (
+  `fid` int(255) NOT NULL,
+  `pid` int(255) NOT NULL,
+  `product` varchar(255) NOT NULL,
+  `pcat` varchar(255) NOT NULL,
+  `pinfo` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `pimage` varchar(255) NOT NULL DEFAULT 'blank.png',
+  `picStatus` int(10) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fproduct`
+--
+
+INSERT INTO `fproduct` (`fid`, `pid`, `product`, `pcat`, `pinfo`, `price`, `pimage`, `picStatus`) VALUES
+(2, 3, 'cereal', 'rice', '<p>this is a cereal</p>', 0, 'rice1.jpg', 5),
+(1, 23, 'tuber', 'potato', '<P>this is a tuber</P>', 300, 'potato.jpg', 3),
+(3, 27, 'Apple', 'Fruit', '<p>This is an Apple</p>\r\n', 500, 'Green Apple4.jpg', 1),
+(3, 28, 'Ladyfinger', 'Vegetable', '<p>Its veggie</p>\r\n', 1000, 'Ladyfinger5.jpg', 1),
+(3, 29, 'Legumes', 'Grains', '<p>these are legumes</p>\r\n', 400, 'grains.jpg', 1),
+(3, 30, 'Vegetables', 'bananas', '<p> Maize</p>\r\n', 400, 'maize5.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likedata`
+--
+
+CREATE TABLE `likedata` (
+  `blogId` int(10) NOT NULL,
+  `blogUserId` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likedata`
+--
+
+INSERT INTO `likedata` (`blogId`, `blogUserId`) VALUES
+(19, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mycart`
+--
+
+CREATE TABLE `mycart` (
+  `bid` int(10) NOT NULL,
+  `pid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mycart`
+--
+
+INSERT INTO `mycart` (`bid`, `pid`) VALUES
+(3, 27),
+(3, 30),
+(0, 0),
+(5, 27),
+(5, 27),
+(5, 30),
+(5, 3),
+(6, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `pid` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `rating` int(10) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `tid` int(10) NOT NULL,
+  `bid` int(10) NOT NULL,
+  `pid` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pincode` varchar(255) NOT NULL,
+  `addr` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`tid`, `bid`, `pid`, `name`, `city`, `mobile`, `email`, `pincode`, `addr`) VALUES
+(1, 3, 28, 'sa,j,cns', 'sajc', 'sajch', 'kmendki98@gmail.com', 'sacu', 'ckaskjc'),
+(2, 6, 3, 'stanley', 'Kiambu', '0702884698', 'stan@gmail.com', '00209', 'enkariak-rongena');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blogdata`
+--
+ALTER TABLE `blogdata`
+  ADD PRIMARY KEY (`blogId`);
+
+--
+-- Indexes for table `buyer`
+--
+ALTER TABLE `buyer`
+  ADD PRIMARY KEY (`bid`),
+  ADD UNIQUE KEY `bid` (`bid`);
+
+--
+-- Indexes for table `farmer`
+--
+ALTER TABLE `farmer`
+  ADD PRIMARY KEY (`fid`),
+  ADD UNIQUE KEY `fid` (`fid`);
+
+--
+-- Indexes for table `fproduct`
+--
+ALTER TABLE `fproduct`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `likedata`
+--
+ALTER TABLE `likedata`
+  ADD KEY `blogId` (`blogId`),
+  ADD KEY `blogUserId` (`blogUserId`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blogdata`
+--
+ALTER TABLE `blogdata`
+  MODIFY `blogId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `buyer`
+--
+ALTER TABLE `buyer`
+  MODIFY `bid` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `farmer`
+--
+ALTER TABLE `farmer`
+  MODIFY `fid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `fproduct`
+--
+ALTER TABLE `fproduct`
+  MODIFY `pid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `tid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `buyer`
+--
+ALTER TABLE `buyer`
+  ADD CONSTRAINT `buyer_ibfk_1` FOREIGN KEY (`bid`) REFERENCES `farmer` (`fid`);
+
+--
+-- Constraints for table `likedata`
+--
+ALTER TABLE `likedata`
+  ADD CONSTRAINT `likedata_ibfk_1` FOREIGN KEY (`blogId`) REFERENCES `blogdata` (`blogId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ menu.php
@@ -0,0 +1,45 @@
+<body style="padding-top: 0px; padding-bottom: 0px;">
+    
+    <!-- Navigation -->
+   
+<header class="jumbotron hero-spacer" style= "background: url(images/banner.jpg); margin-top: 0px; background-size: cover; height: 400px;">
+    <nav class="navbar navbar-inverse navbar-fixed-top"style= "opacity: 0.7; filter:alpha (opacity =70);" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header"  >
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+              <a class="navbar-brand" href="index.php" style = "padding-right: 100px; font-size: 25px; background-color: blue; color: black;"><b>CommercialFarm.com</b></a>
+
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+                
+                	<ul class="nav navbar-nav"  >
+					<li>
+                        <a href="index.php" style= "font-weight: bold;padding-right: 80px;" >Home</a>
+                    </li>
+					
+					<li>
+                        <a href="market.php" style= "font-weight: bold;padding-right: 80px;" >Buy Farm Products</a>
+                    </li>
+					<li>
+                        <a href="market.php" style= "font-weight: bold;padding-right: 80px;" >Login As Farmer</a>
+                    </li>
+					<li>
+                        <a href="blogView" style= "font-weight: bold;padding-right: 50px;" >How it Works</a>
+                    </li>
+					
+				<li>
+					<a  href = "myCart.php" style = " font-weight: bold; padding-right: 50px;"> myCart</a>
+				</li>
+				
+					
+				</ul>
+            </div>
+            
+	 </header>   
